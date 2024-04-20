b9091548fc8d67ee0302cddaa91787299f2c8396
rish727 <ishiyama.ryo.727@gmail.com>
2024-04-20 22:46:14 +0900

    refactoring

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ c = Camera()
 HOST = '192.168.40.102'
 PORT = 10000
 AUDIO_DEVICE = 'hw:0,0' 
+
 sclient = Client(HOST, PORT)
 aud = Audio(AUDIO_DEVICE)
 
@@ -19,49 +20,33 @@ start = time.time()
 interval_sec = 5
 sleep_time = 5
 
-""" 
-while True:
-    frame = c.capture()
-    # c.show(frame)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
-    
-    if time() - start > interval_sec:
-        ## save frame as image
-        # cv2.imwrite("out.jpeg", frame)
-        
-        # print("Capturing image...")
-        start = time()
-        text = it.run("out.jpeg")
-        names, probs = y.get_results("out.jpeg")
-
-
-        print(text)
-        break
-"""
-
-while True:
-    aud.speack_ojtalk("まわりを読み込むきゅぶ", "f")
-    time.sleep(aud.out_duration + 0.3)
-    frame = c.caputure("out.jpg")
-
-    aud.speack_ojtalk("ふむふむ", "f")
-    time.sleep(aud.out_duration + 0.3)
-    out = sclient.send_image("out.jpg")
-    text = out.decode()
-    # aud.speack_ojtalk(text, "f")
-    
-    
-    print(text)
-    # out = "cup"
-    if "cup" in text or "mug" in text:
-        aud.speack_ojtalk("カップンが検知されましたきゅぶ．ヘッドマウントディスプレイを装着して様子を見てみるきゅぶ", "f")
+def main():
+    while True:
+        aud.speack_ojtalk("まわりを読み込むきゅぶ", "f")
         time.sleep(aud.out_duration + 0.3)
-    # text = it.run("out.jpg")
-    # print(text)
-    print(time.time() - start)
+        frame = c.caputure("out.jpg")
 
-    time.sleep(sleep_time)
+        aud.speack_ojtalk("ふむふむ", "f")
+        time.sleep(aud.out_duration + 0.3)
+        out = sclient.send_image("out.jpg")
+        text = out.decode()
+        # aud.speack_ojtalk(text, "f")
+        
+        
+        print(text)
+        # out = "cup"
+        if "cup" in text or "mug" in text:
+            aud.speack_ojtalk("カップンが検知されましたきゅぶ．ヘッドマウントディスプレイを装着して様子を見てみるきゅぶ", "f")
+            time.sleep(aud.out_duration + 0.3)
+        # text = it.run("out.jpg")
+        # print(text)
+        print(time.time() - start)
+
+        time.sleep(sleep_time)
 
 # a close - up view of a white wall . 
 # 231.3489875793457
+
+
+if __name__ == "__main__":
+    main()
