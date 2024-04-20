93aacedf78a5f05b4e602bb125eec62d00d7a821
rish727 <ishiyama.ryo.727@gmail.com>
2024-04-20 22:36:47 +0900

    add audio instance

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import time
 from camera.model.coca import Image2Text
 from camera.utils.camera import Camera
 from wsocket.client import Client
+from audio.audio import Audio
 
 c = Camera()
 # it = Image2Text()
 
 HOST = '192.168.40.102'
 PORT = 10000
+AUDIO_DEVICE = 'hw:0,0' 
 sclient = Client(HOST, PORT)
+aud = Audio(AUDIO_DEVICE)
 
 start = time.time()
 interval_sec = 5
@@ -38,8 +41,22 @@ while True:
 """
 
 while True:
+    aud.speack_ojtalk("まわりを読み込むきゅぶ", "f")
+    time.sleep(aud.out_duration + 0.3)
     frame = c.caputure("out.jpg")
-    sclient.send_image("out.jpg")
+
+    aud.speack_ojtalk("ふむふむ", "f")
+    time.sleep(aud.out_duration + 0.3)
+    out = sclient.send_image("out.jpg")
+    text = out.decode()
+    # aud.speack_ojtalk(text, "f")
+    
+    
+    print(text)
+    # out = "cup"
+    if "cup" in text or "mug" in text:
+        aud.speack_ojtalk("カップンが検知されましたきゅぶ．ヘッドマウントディスプレイを装着して様子を見てみるきゅぶ", "f")
+        time.sleep(aud.out_duration + 0.3)
     # text = it.run("out.jpg")
     # print(text)
     print(time.time() - start)
